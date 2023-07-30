196df3f7e31df24f86f2e0558fa37800a9064465
MTofayelAhmed <tofayelahmed324@gmail.com>
2023-07-31 01:13:48 +0600

    done with blog page

--- src/app/blog/page.js
+++ src/app/blog/page.js
@@ -4,6 +4,7 @@ const BlogPage = () => {
     return (
         <div>
             <h1>this is a blogPage</h1>
+            <h2>this a blog page for showing dynamic routes </h2>
         </div>
     );
 };
