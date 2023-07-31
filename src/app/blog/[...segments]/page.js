c5a38723ae1141623bab00b57e3fd972400e1d0b
MTofayelAhmed <tofayelahmed324@gmail.com>
2023-07-31 12:33:06 +0600

    done with nexted routes

---
+++ src/app/blog/[...segments]/page.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const SingleBlog = ({params}) => {
+    console.log(params)
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default SingleBlog;
