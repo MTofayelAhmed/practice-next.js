970e1e0330852c7f5a6d3e029a0751258b4ffe7b
MTofayelAhmed <tofayelahmed324@gmail.com>
2023-08-02 13:59:31 +0600

    done with image

--- src/app/(with-layout)/page.js
+++ src/app/(with-layout)/page.js
@@ -1,7 +1,16 @@
+import salad from '@/assets/salad.jpg'
+import Image from 'next/image';
+
+
 const HomePage = () => {
   return (
     <main>
       <h2>Home</h2>
+    <Image src={salad} placeholder='blur' alt='' width={400}  height={400}></Image>
+    {/* <Image src='/salad.jpg' placeholder='blur'blurDataURL='' alt='' width={400}  height={400}></Image> */}
+    {/* (image upload kore 
+    generate kore nite hoeb) */}
+    {/* <Image src="https://www.freepik.com/free-photo/front-view-women-with-bicycles_32338588.htm#from_view=detail_serie" alt='' width={400}  height={400}></Image> */}
     </main>
   );
 };
