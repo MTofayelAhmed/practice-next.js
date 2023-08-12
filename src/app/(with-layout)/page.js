b0c75cc3ee2ae657db8d7e9db0c09113ba0cc2ec
MTofayelAhmed <tofayelahmed324@gmail.com>
2023-08-12 19:29:06 +0600

    done with commit

--- src/app/(with-layout)/page.js
+++ src/app/(with-layout)/page.js
@@ -1,5 +1,7 @@
 
 import Categories from '@/components/Homepage/Categories';
+import PopularProducts from '@/components/Homepage/PopularProducts';
+import { Suspense } from 'react';
 export const revalidate = 5
 
 const HomePage = () => {
@@ -7,6 +9,10 @@ const HomePage = () => {
     <main>
       <h2 className=' text-red-600'>Home</h2>
    <Categories></Categories>
+   <Suspense fallback={<h1 className="text-center font-semibold text-2xl">Loading...</h1>  }>
+   <PopularProducts></PopularProducts>
+   </Suspense>
+   
     </main>
   )
 };
---
+++ src/components/Homepage/PopularProducts.jsx
@@ -0,0 +1,14 @@
+import wait from "@/utils/wait";
+
+
+const PopularProducts = async() => {
+await wait()
+    return (
+        <div className=" mt-10">
+           <h1>This is popular product section</h1> 
+
+        </div>
+    );
+};
+
+export default PopularProducts;
